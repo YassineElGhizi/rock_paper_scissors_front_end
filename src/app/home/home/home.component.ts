2ae1511d074f75e563ce759bb6514bbd0b292ebe
Yassineelghizi <yassine.ghizi2@gmail.com>
2022-09-05 18:39:24 +0100

    persistance DOne

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../../login/service/login.service";
 import {Router} from "@angular/router";
+import {select, Store} from "@ngrx/store";
+import {UserState} from "../../login/store/reducer/login.reducer";
+import {Observable} from "rxjs";
+import {User} from "../../models/user";
+import {selectUsers} from "../../login/store/selector/login.selectors";
+import {addUser} from "../../login/store/action/login.actions";
 
 @Component({
   selector: 'app-home',
@@ -10,12 +16,49 @@ import {Router} from "@angular/router";
 export class HomeComponent implements OnInit {
 
   authenticated = false
+  public user$: Observable<User>;
 
-  constructor(private loginService: LoginService, public router: Router) {
+  constructor(private loginService: LoginService, public router: Router, public store: Store<UserState>) {
     this.loginService.authenticated.subscribe(auth => this.authenticated = auth);
+    this.user$ = this.store.pipe(select(selectUsers));
   }
 
   ngOnInit(): void {
+    this.user$.subscribe(
+      user => {
+
+        let my_user;
+        let id;
+        let img;
+        let name;
+        let token;
+        if (user.id) {
+          localStorage.setItem("id", String(user.id));
+          localStorage.setItem("img", <string>user.img);
+          localStorage.setItem("token", <string>user.token);
+          localStorage.setItem("name", <string>user.name);
+        } else {
+          id = localStorage.getItem("id")
+          img = localStorage.getItem("img")
+          token = localStorage.getItem("token")
+          name = localStorage.getItem("name")
+
+          my_user = new User()
+          my_user.id = Number(id)
+          my_user.img = String(img)
+          my_user.token = String(token)
+          my_user.name = String(name)
+
+          this.store.dispatch(addUser(my_user))
+          this.authenticated = true
+        }
+      }
+    )
+
+  }
+
+  public save_data = () => {
+
   }
 
 }
